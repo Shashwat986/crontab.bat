cd258a03748c2bffe97debd6e710fea28bf03ddc
Shashwat986 <chandra.shashwat@gmail.com>
2014-08-10 03:18:32 +0530

    Added description for new users
    TODO: Change editor settings

--- crontab.bat
+++ crontab.bat
@@ -1,7 +1,32 @@
+@echo off
 if exist {%UserProfile%\.crontab} (
 	echo edit existing crontab for %UserName%
-	vim .crontab
+	vim %UserProfile%\.crontab
 ) else (
 	echo no crontab for %UserName%
-	vim .crontab
+
+	echo # Edit this file to introduce tasks to be run by cron. > .crontab
+	echo #  >> .crontab
+	echo # Each task to run has to be defined through a single line >> .crontab
+	echo # indicating with different fields when the task will be run >> .crontab
+	echo # and what command to run for the task >> .crontab
+	echo #  >> .crontab
+	echo # To define the time you can provide concrete values for >> .crontab
+	echo # minute ^(m^), hour ^(h^), day of month ^(dom^), month ^(mon^), >> .crontab
+	echo # and day of week ^(dow^) or use '*' in these fields ^(for 'any'^).#  >> .crontab
+	echo # Notice that tasks will be started based on the cron's system >> .crontab
+	echo # daemon's notion of time and timezones. >> .crontab
+	echo #  >> .crontab
+	echo # Output of the crontab jobs ^(including errors^) is sent through >> .crontab
+	echo # email to the user the crontab file belongs to ^(unless redirected^). >> .crontab
+	echo #  >> .crontab
+	echo # For example, you can run a backup of all your user accounts >> .crontab
+	echo # at 5 a.m every week with: >> .crontab
+	echo # 0 5 * * 1 tar -zcf /var/backups/home.tgz /home/ >> .crontab
+	echo #  >> .crontab
+	echo # For more information see the manual pages of crontab^(5^) and cron^(8^) >> .crontab
+	echo #  >> .crontab
+	echo # m h  dom mon dow   command >> .crontab
+
+	vim %UserProfile%\.crontab
 )
